cac6a74392c363de58f629dae741ab79a7920a28
Tretyakov Maksim <m.tretyakov@skbkontur.ru>
2025-12-27 18:45:14 +0500

    fix lint

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -79,86 +79,85 @@ function FavoritesPage(): JSX.Element {
                         </div>
                       </div>
                       <div className="favorites__places">
-                        {groupedOffers[city].map((offer) => (
-                          <article
-                            className="favorites__card place-card"
-                            key={offer.id}
-                          >
-                            {offer.isPremium && (
-                              <div className="place-card__mark">
-                                <span>Premium</span>
-                              </div>
-                            )}
-                            <div className="favorites__image-wrapper place-card__image-wrapper">
-                              <Link to={`/offer/${offer.id}`}>
-                                <img
-                                  className="place-card__image"
-                                  src={offer.previewImage}
-                                  width="150"
-                                  height="110"
-                                  alt={offer.title}
-                                />
-                              </Link>
-                            </div>
-                            <div className="favorites__card-info place-card__info">
-                              <div className="place-card__price-wrapper">
-                                <div className="place-card__price">
-                                  <b className="place-card__price-value">
-                                    &euro;{offer.price}
-                                  </b>
-                                  <span className="place-card__price-text">
-                                    &#47;&nbsp;night
-                                  </span>
+                        {groupedOffers[city].map((offer) => {
+                          const handleBookmarkOfferClick = () =>
+                            handleBookmarkClick(offer.id, offer.isFavorite);
+                          return (
+                            <article
+                              className="favorites__card place-card"
+                              key={offer.id}
+                            >
+                              {offer.isPremium && (
+                                <div className="place-card__mark">
+                                  <span>Premium</span>
                                 </div>
-                                <button
-                                  className={`place-card__bookmark-button ${
-                                    offer.isFavorite
-                                      ? 'place-card__bookmark-button--active'
-                                      : ''
-                                  } button`}
-                                  type="button"
-                                  onClick={() =>
-                                    handleBookmarkClick(
-                                      offer.id,
-                                      offer.isFavorite
-                                    )
-                                  }
-                                >
-                                  <svg
-                                    className="place-card__bookmark-icon"
-                                    width="18"
-                                    height="19"
-                                  >
-                                    <use xlinkHref="#icon-bookmark"></use>
-                                  </svg>
-                                  <span className="visually-hidden">
-                                    In bookmarks
-                                  </span>
-                                </button>
-                              </div>
-                              <div className="place-card__rating rating">
-                                <div className="place-card__stars rating__stars">
-                                  <span
-                                    style={{
-                                      width: `${
-                                        Math.round(offer.rating) * 20
-                                      }%`,
-                                    }}
-                                  />
-                                  <span className="visually-hidden">
-                                    Rating
-                                  </span>
-                                </div>
-                              </div>
-                              <h2 className="place-card__name">
+                              )}
+                              <div className="favorites__image-wrapper place-card__image-wrapper">
                                 <Link to={`/offer/${offer.id}`}>
-                                  {offer.title}
+                                  <img
+                                    className="place-card__image"
+                                    src={offer.previewImage}
+                                    width="150"
+                                    height="110"
+                                    alt={offer.title}
+                                  />
                                 </Link>
-                              </h2>
-                              <p className="place-card__type">{offer.type}</p>
-                            </div>
-                          </article>
-                        ))}
+                              </div>
+                              <div className="favorites__card-info place-card__info">
+                                <div className="place-card__price-wrapper">
+                                  <div className="place-card__price">
+                                    <b className="place-card__price-value">
+                                      &euro;{offer.price}
+                                    </b>
+                                    <span className="place-card__price-text">
+                                      &#47;&nbsp;night
+                                    </span>
+                                  </div>
+                                  <button
+                                    className={`place-card__bookmark-button ${
+                                      offer.isFavorite
+                                        ? 'place-card__bookmark-button--active'
+                                        : ''
+                                    } button`}
+                                    type="button"
+                                    onClick={handleBookmarkOfferClick}
+                                  >
+                                    <svg
+                                      className="place-card__bookmark-icon"
+                                      width="18"
+                                      height="19"
+                                    >
+                                      <use xlinkHref="#icon-bookmark"></use>
+                                    </svg>
+                                    <span className="visually-hidden">
+                                      In bookmarks
+                                    </span>
+                                  </button>
+                                </div>
+                                <div className="place-card__rating rating">
+                                  <div className="place-card__stars rating__stars">
+                                    <span
+                                      style={{
+                                        width: `${
+                                          Math.round(offer.rating) * 20
+                                        }%`,
+                                      }}
+                                    />
+                                    <span className="visually-hidden">
+                                      Rating
+                                    </span>
+                                  </div>
+                                </div>
+                                <h2 className="place-card__name">
+                                  <Link to={`/offer/${offer.id}`}>
+                                    {offer.title}
+                                  </Link>
+                                </h2>
+                                <p className="place-card__type">{offer.type}</p>
+                              </div>
+                            </article>
+                          );
+                        })}
                       </div>
                     </li>
                   ))}
